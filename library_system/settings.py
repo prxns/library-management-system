81ffd84f9e225ecf31eef4e7e71c7f6c35aefd86
prxns <pranshurwt2003@gmail.com>
2026-04-14 23:09:01 +0530

    Fixed username and password

--- library_system/settings.py
+++ library_system/settings.py
@@ -50,8 +50,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "library_db",
-        "USER": "library_user",
-        "PASSWORD": "StrongPass123!",
+        "USER": "Pranshu",
+        "PASSWORD": "Destiny2308",
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "OPTIONS": {
