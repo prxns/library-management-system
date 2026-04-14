db7ef4bf4c4d544a43b7ec3cc62606cabfb64ec7
prxns <pranshurwt2003@gmail.com>
2026-04-14 23:10:52 +0530

    Fixed bugs and added necessary files

---
+++ library_system/asgi.py
@@ -0,0 +1,10 @@
+"""
+ASGI config for library_system project.
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_system.settings")
+
+application = get_asgi_application()
